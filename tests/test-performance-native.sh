475166b3eb9aa49c96e673531cf9bf7ae8506293
GeoDimi99 <dimitrov.georgi.t@gmail.com>
2026-03-18 16:46:12 +0000

    Complete test script

--- tests/test-performance-native.sh
+++ tests/test-performance-native.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 # Set env variables
-METRICS_FILENAME=$1
+TEST_NAME=$1
+
+TESTS_DIR=$(pwd)
+
+cd ../services/execution-manager/
+cmake --build build
+cd $TESTS_DIR
+
+mkdir $TEST_NAME || rm -rf $TEST_NAME && mkdir $TEST_NAME
+cd $TEST_NAME
+echo Qui $(pwd)
 
 # Init the file table fields
-echo "iteration,task_id,start_req,end_req,start_res,end_res,em_tw_ms,task_ms,tw_em_ms,total_ms" > $METRICS_FILENAME
+echo "iteration,task_id,start_req,end_req,start_res,end_res,em_tw_ms,task_ms,tw_em_ms,total_ms" > $TEST_NAME.csv
 
 # Run the program with performance sensors
-/home/vboxuser/projects/RT-microservices-native/services/execution-manager/build/execution-manager | tee output.log
-grep "PERF_LOG:" output.log | sed 's/PERF_LOG://' >> $METRICS_FILENAME
+../../services/execution-manager/build/execution-manager | tee output.log
+grep "PERF_LOG:" output.log | sed 's/PERF_LOG://' >> $TEST_NAME.csv
 
-python3 analyze_performance.py $METRICS_FILENAME
+python3 ../analyze_performance.py $TEST_NAME.csv
